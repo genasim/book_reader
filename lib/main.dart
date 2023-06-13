2cf63f43f6daf52100e3e64966461b67e2973547
Genadi Kolev <genadimkolev@gmail.com>
2023-06-13 16:28:54 +0300

    login page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:book_reader/screens/login_screen.dart';
+import 'package:book_reader/size_data.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,14 +12,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BookReader',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    configSizeData(context);
+
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          FocusManager.instance.primaryFocus?.unfocus();
+        }
+      },
+      child: MaterialApp(
+        title: 'BookReader',
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme:
+              const AppBarTheme(color: Colors.transparent, elevation: 0),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+          useMaterial3: true,
+        ),
+        home: const LoginScreen(),
       ),
-      home: const Placeholder(),
     );
   }
 }
